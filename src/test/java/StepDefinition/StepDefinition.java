073da5f33f802692d02b87a00f95f0c832bcc8af
radebelerato@gmail.com <radebelerato@gmail.com>
2024-11-24 08:51:25 +0200

    Add a new feature

--- src/test/java/StepDefinition/StepDefinition.java
+++ src/test/java/StepDefinition/StepDefinition.java
@@ -1,4 +1,12 @@
 package StepDefinition;
 
-public class StepDefinition {
+import io.cucumber.java.en.Given;
+import utility.BrowserDriver;
+
+public class StepDefinition extends BrowserDriver {
+
+    @Given("Automation exercise webpage is launched")
+    public void launchwebpage(){
+        driver.get("www.automationexercise.com");
+    }
 }
